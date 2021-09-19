a242682511eaefaf291c2087f9efd7b5cc021f24
Rasm-P <rasmus.pra@gmail.com>
2021-09-19 17:06:54 +0200

    Created a working project with REST api and ORM working with a MySQL database

---
+++ PayManWebAPI/Controllers/TodoItemController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PayManWebAPI.DBContexts;
+using PayManWebAPI.Models;
+
+namespace PayManWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemController : ControllerBase
+    {
+        private MyDBContext myDbContext;
+
+        public TodoItemController(MyDBContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<TodoItem> Get()
+        {
+            return (this.myDbContext.TodoItems.ToList());
+        }
+    }
+}
---
+++ PayManWebAPI/Migrations/20210919131026_DBInit.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PayManWebAPI.Migrations
+{
+    public partial class DBInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsComplete = table.Column<sbyte>(type: "tinyint", nullable: false, defaultValue: (sbyte)0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "Idx_Name",
+                table: "TodoItems",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
