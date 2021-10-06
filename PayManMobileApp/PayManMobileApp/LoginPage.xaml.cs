9c7c50e9ad7489c98eab5205a81b00083669112e
Rasm-P <rasmus.pra@gmail.com>
2021-10-06 15:40:29 +0200

    Login

---
+++ PayManMobileApp/PayManMobileApp/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PayManMobileApp
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void TapGestureRecognizer_tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
